ba711ef6ead01e65cd566afc3660ef600d747299
jfabregas <jafabregas0@misena.edu.co>
2016-04-19 07:20:18 -0500

    Added files via upload

---
+++ Base de Datos.sql
@@ -0,0 +1,12 @@
+create database Fiestadecolores;
+use Fiestadecolores;
+create table clientes(
+cedula bigint,
+edad int,
+nombres varchar(20),
+apellidos varchar(30),
+direccion varchar(20),
+telefono int,
+celular bigint,
+correo varchar(20),
+primary key (cedula));
